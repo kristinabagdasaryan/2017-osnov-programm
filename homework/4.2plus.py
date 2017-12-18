44723ec2711f0b4745351b82470b0061fdb65c20
kristinabagdasaryan <kristinabagdasaryan14@gmail.com>
2017-12-18 12:18:39 +0300

    Add files via upload

---
+++ homework/4.2plus.py
@@ -0,0 +1,45 @@
+import json
+import re
+text = open('POS.txt')
+text = text.read().split('\n')
+
+result = []
+readstates = []
+word_dict = {}
+nTokens = 0
+POS_dict = {}
+for state in text:
+    readstates.append(json.loads(state))
+
+for state in readstates:
+    nTokens += len(state)
+    for word in state:
+        key = word[0] + '_' + word[2]
+        if key in word_dict:
+            word_dict[key] += 1
+        else:
+            word_dict[key] = 1
+
+        if word[0] in POS_dict:
+            POS_dict[word[0]] += 1
+        else:
+            POS_dict[word[0]] = 1
+
+
+for token in word_dict:
+    pos, wordform = token.split('_')
+    count = word_dict[token]
+    P = count/nTokens
+    result.append([P, count, pos, wordform])
+
+for pos in POS_dict:
+    pos_count = POS_dict[pos]
+    PPOS = pos_count/nTokens
+    result.append([PPOS, pos_count, pos, '-'])
+
+#print(word_dict)
+
+for q in result:
+    print(q)
+
+
---
+++ homework/tokenizer_without_files.py
@@ -0,0 +1,76 @@
+import re
+
+text = open('wiki.txt', encoding='utf-8-sig')
+text = text.read()
+text = re.sub('  ',' ',text)
+text = re.sub('\\n\\n',' ',text)
+#print(set(text))
+sentences = re.split(' *[\.\?!][\'"\)\]]* *', text) #CLASS2
+
+
+row_tok_sents = []
+tok_sents = [] #CLASS3
+let_dict = {'а': 'a',
+            'б': 'b',
+            'в': 'v',
+            'г': 'g',
+            'д': 'd',
+            'е': 'e',
+            'ё': 'jo',
+            'ж': 'zh',
+            'з': 'z',
+            'и': 'i',
+            'й': 'i',
+            'к': 'k',
+            'л': 'l',
+            'м': 'm',
+            'н': 'n',
+            'о': 'o',
+            'п': 'p',
+            'р': 'r',
+            'с': 's',
+            'т': 't',
+            'у': 'u',
+            'ф': 'f',
+            'х': 'kh',
+            'ц': 'c',
+            'ч': 'ch',
+            'ш': 'sh',
+            'щ': 'shch',
+            'ъ': '″',
+            'ы': 'y',
+            'ь': '′',
+            'э': 'e',
+            'ю': 'ju',
+            'я': 'ya'}
+
+for sent in sentences:
+    row_tok_sents.append(sent.split())
+
+for toksent in row_tok_sents:
+    i = 1
+    inxtoksent = []
+    for word in toksent:
+        transword = ''
+        for letter in word:
+            if letter.lower() in let_dict:
+                transword += let_dict[letter.lower()]
+            else:
+                transword += letter
+        inxtoksent.append([i, word, transword])
+        i += 1
+    tok_sents.append(inxtoksent) #CLASS3
+
+t = 0
+for q in tok_sents:
+    if t < 100:
+        print(q)
+        t += len(q)
+    else:
+        break
+
+
+
+
+
+
